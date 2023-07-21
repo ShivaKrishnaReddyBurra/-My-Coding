74a11ad5c381a5d24ffe785afd6b33c58fbbfb3e
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2023-07-21 13:38:18 +0530

    Sum of numbers
    Added simple addition code

---
+++ basic codes/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,sum;
+    printf("Enter the number_1 :");
+    scanf("%d",&a);
+    printf("Enter the number_2 :");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The Sum of Two digits is : %d",&sum);
+
+}
