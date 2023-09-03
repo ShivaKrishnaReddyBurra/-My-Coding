332af695132caa4f175efdadc70ff18ff0304d73
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2023-09-03 10:28:59 +0530

    Code for fibonacci series
    changed

--- fibonacci series/code for fibonacci series.cpp
+++ fibonacci series/code for fibonacci series.cpp
@@ -4,11 +4,11 @@ int main()
  	int a=0,b=1,number,nextnumber,i;
  	printf("Enter the number upto were you want to print fibonacci series \n");
  	scanf("%d",&number);
- 	printf("Fibonacci series is : %d,%d,",a,b);
+ 	printf("Fibonacci series is : %d ,%d ,",a,b);
  	nextnumber=a+b;
  	for(i=3;nextnumber<number;i++)
  	{
- 		printf("%d,",nextnumber);
+ 		printf("%d ,",nextnumber);
  		a=b;
  		b=nextnumber;
  		nextnumber=a+b;
